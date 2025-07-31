e1e0583a3b4644fd148c335b74d3e4e3c21db7ee
Joaquin Calligaro <joaquincalligaro@gmail.com>
2025-07-31 20:28:24 -0300

    Bugs Fixed

--- src/App.tsx
+++ src/App.tsx
@@ -10,35 +10,36 @@ function App() {
   const [tareas, setTareas] = useState<string[]>([]);
   //! Aqui se renderiza la UI (Toda la interfaz Grafica)
   return (
-    <div className="flex gap-2 mb-4 ">
-      <input
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        className="border rounded p-2 flex-1 "
-        type="text"
-        placeholder="Escribi tu tarea..."
-      />
-      <button
-        onClick={() => {
-          if (input.trim() !== '') {
-            setTareas([...tareas, input]);
-            setInput('');
-          }
-        }}
-        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 hover:text-blue-600 ml-2">
-        Agregar
-      </button>
-
-      {/* 7. Lista de tareas */}
-      <ul>
-        {tareas.map((tarea, i) => (
-          <li key={i} className="border-b py-2">
-            {tarea}{' '}
-          </li>
-        ))}
-      </ul>
+    <div className="gap-2 mb-4 ">
+      <div className="flex">
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="border rounded p-2 flex-1 "
+          type="text"
+          placeholder="Escribi tu tarea..."
+        />
+        <button
+          onClick={() => {
+            if (input.trim() !== '') {
+              setTareas([...tareas, input]);
+              setInput('');
+            }
+          }}
+          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 hover:text-blue-600 ml-2">
+          Agregar
+        </button>
+      </div>
+      <div className="flex-col">
+        <ul>
+          {tareas.map((tarea, i) => (
+            <li key={i} className=" border-b py-2">
+              {tarea}{' '}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
-
 export default App;
